888c628c684b68ed93be073e6b0e64430a8be4af
Zaljun <50000626+Zaljun@users.noreply.github.com>
2020-06-16 23:39:38 -0400

    Add files via upload

--- Ch2/End2End.py
+++ Ch2/End2End.py
@@ -292,4 +292,59 @@ joblib.dump(forest_reg, "my_model.pkl")
 my_model_loaded = joblib.load("my_model.pkl")
 
 # FINETUNE MODEL
+# GridSearch for Forest
+from sklearn.model_selection import GridSearchCV
 
+param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+        ]
+
+forest_reg  = RandomForestRegressor()
+
+grid_search = GridSearchCV(forest_reg, param_grid, cv = 5,
+                           scoring = 'neg_mean_squared_error',
+                           return_train_score = True)
+
+grid_search.fit(housing_prepared, housing_labels)
+grid_search.best_estimator_ 
+grid_search.best_params_ # {'max_features': 6, 'n_estimators': 30}
+# Notice taht 8 is the maxism, which means you might need try bigger value
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
+
+# ANALYZE the BEST MODELS and THEIR ERRORS
+feature_importances = grid_search.best_estimator_.feature_importances_
+feature_importances
+
+# Display importances score next to corresponding attributes
+extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+cat_encoder   = full_pipeline.named_transformers_["cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attributes    = num_attribs + extra_attribs + cat_one_hot_attribs
+sorted(zip(feature_importances, attributes), reverse = True)
+# With These IMPORTANCES, can MODIFY (Drop or Add) features
+# and TRY
+
+# EVALUATE SYSTEM ON TEST SET
+final_model = grid_search.best_estimator_
+
+X_test = strat_test_set.drop("median_house_value", axis =1)
+y_test = strat_test_set["median_house_value"].copy()
+
+X_test_prepared = full_pipeline.transform(X_test)
+#X_test_prepared = full_pipeline.fit_transform(X_test)
+
+final_predictions = final_model.predict(X_test_prepared)
+
+final_mse  = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+final_rmse
+
+from scipy import stats
+confidence = 0.95
+squared_errors = (final_predictions - y_test) ** 2
+np.sqrt(stats.t.interval(confidence, len(squared_errors) -1,
+                       loc = squared_errors.mean(),
+                       scale = stats.sem(squared_errors)))
